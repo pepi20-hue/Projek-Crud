84845762186b64c647d5ee293c52e794722dfa2a
fikz31 <fikri311002@gmail.com>
2024-08-13 10:35:23 +0800

    menambah spasi

--- login.php
+++ login.php
@@ -6,7 +6,7 @@ require "db/function.php";
 // Check if form is submitted
 if(isset($_POST['login'])){
     $username = $_POST['username'];
-    $password = $_POST['password'];
+      $password = $_POST['password'];
 
     // Validate email and password are not empty
     if (!empty($username) && !empty($password)) {
